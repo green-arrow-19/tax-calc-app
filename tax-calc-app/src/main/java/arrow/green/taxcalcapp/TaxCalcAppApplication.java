65a4185f1e0f8be90229c620a522c8d2e6a0756f
nakulGoyal19 <nakul19goyal@gmail.com>
2020-08-28 13:20:02 +0530

    Initial project commit

---
+++ tax-calc-app/src/main/java/arrow/green/taxcalcapp/TaxCalcAppApplication.java
@@ -0,0 +1,13 @@
+package arrow.green.taxcalcapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxCalcAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxCalcAppApplication.class, args);
+	}
+
+}
---
+++ tax-calc-app/src/test/java/arrow/green/taxcalcapp/TaxCalcAppApplicationTests.java
@@ -0,0 +1,13 @@
+package arrow.green.taxcalcapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxCalcAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
