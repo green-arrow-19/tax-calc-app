6d0438690f1378a87685de58d161fe315f6a7a49
nakulGoyal19 <nakul19goyal@gmail.com>
2020-08-29 14:22:27 +0530

    Signup flow

--- tax-calc-app/src/main/java/arrow/green/taxcalcapp/controller/UserController.java
+++ tax-calc-app/src/main/java/arrow/green/taxcalcapp/controller/UserController.java
@@ -1,11 +1,39 @@
 package arrow.green.taxcalcapp.controller;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import arrow.green.taxcalcapp.model.SignUpRequest;
+import arrow.green.taxcalcapp.model.SignUpResponse;
+import arrow.green.taxcalcapp.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author nakulgoyal
  *         28/08/20
  **/
+
+@Slf4j
+@RestController
+@RequestMapping("/user")
 public class UserController {
-
+    
+    @Autowired
+    UserService userService;
+    
+    @PostMapping("/signup")
+    public ResponseEntity<SignUpResponse> signup(@Valid @RequestBody SignUpRequest signUpRequest,
+            HttpServletResponse httpServletResponse) {
+        log.info("SignUp request for user : {}", signUpRequest.getUsername());
+        SignUpResponse signUpResponse = userService.signup(signUpRequest, httpServletResponse);
+        log.info("SignUp request SUCCESSFUL for user : {}", signUpRequest.getUsername());
+        return ResponseEntity.accepted().body(signUpResponse);
+    }
 }
-
 
--- tax-calc-app/src/main/java/arrow/green/taxcalcapp/repository/UserRepository.java
+++ tax-calc-app/src/main/java/arrow/green/taxcalcapp/repository/UserRepository.java
@@ -1,10 +1,16 @@
 package arrow.green.taxcalcapp.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import arrow.green.taxcalcapp.model.User;
+
 /**
  * @author nakulgoyal
  *         28/08/20
  **/
-public class UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 
 }
 
