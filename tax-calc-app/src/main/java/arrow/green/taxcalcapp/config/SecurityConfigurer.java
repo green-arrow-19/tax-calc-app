bc910d4f04949817c6104765adfcea8e35f3bbf7
nakulGoyal19 <nakul19goyal@gmail.com>
2020-09-01 00:47:02 +0530

    Configured swagger related endpoints

--- tax-calc-app/src/main/java/arrow/green/taxcalcapp/config/SecurityConfigurer.java
+++ tax-calc-app/src/main/java/arrow/green/taxcalcapp/config/SecurityConfigurer.java
@@ -2,13 +2,11 @@ package arrow.green.taxcalcapp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -36,7 +34,8 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-            .authorizeRequests().antMatchers("/user/signup", "/user/signin").permitAll()
+            .authorizeRequests().antMatchers("/user/signup", "/user/signin", "/swagger-ui.html", "/actuator/**", "/webjars/**", "/swagger-resources/**", "/v2/api-docs",
+                                             "/h2/**", "/favicon.ico", "/").permitAll()
             .anyRequest().authenticated()
             .and().sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
