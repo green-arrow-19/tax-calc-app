78d57b840a1a42eea23e284f95b38a4922ad60d7
nakulGoyal19 <nakul19goyal@gmail.com>
2020-09-04 22:26:16 +0530

    Items and constant tax amounts.

---
+++ tax-calc-app/src/main/java/arrow/green/taxcalcapp/model/TaxItem.java
@@ -0,0 +1,24 @@
+package arrow.green.taxcalcapp.model;
+
+/**
+ * @author nakulgoyal
+ *         04/09/20
+ **/
+public enum TaxItem {
+    FOOD, ELECTRONICS, CLOTHES, AUTO_MOBILE, NO_TAX, OTHERS
+
+}
+
+/**
+ * @author nakulgoyal
+ *         04/09/20
+ **/
+class TaxAmountConstants {
+    public final static Double FOOD = 5.0;
+    public final static Double ELECTRONICS = 5.0;
+    public final static Double CLOTHES = 5.0;
+    public final static Double AUTO_MOBILE = 5.0;
+    public final static Double NO_TAX = 0.0;
+    public final static Double OTHERS = 1.0;
+    
+}
